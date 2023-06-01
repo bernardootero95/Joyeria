493ed9b972691039c3c935b6deebbcb98ae8ecdb
Bernardo otero <bernardooteroaj@gmail.com>
2023-06-01 00:00:53 -0500

    cuarto correccioncarga y calculo valor

--- src/components/formulario.jsx
+++ src/components/formulario.jsx
@@ -1,29 +1,52 @@
 import React, { useEffect, useState } from 'react'
+import { db } from '../Firebase';
+import { collection, doc, addDoc, onSnapshot, deleteDoc, updateDoc } from 'firebase/firestore'
+
+
 
 const Formulario = () => {
-    const [material, setMaterial] = useState('')
-    const [dije, setDije] = useState('')
-    const [tipo, setTipo] = useState('')
-    const [joyas, setJoyas] = useState([])
+    const [cantidad, setCantidad] = useState('');
+    const [material, setMaterial] = useState('');
+    const [dije, setDije] = useState('');
+    const [tipo, setTipo] = useState('');
+    const [moneda, setMoneda] = useState('');
+    const [valorAPagar, setValorAPagar] = useState('');
+    const [listarJoyas, setListarJoyas] = useState([])
+  
+    useEffect(() => {
+      const obtenerDatos = async() =>{
+        try{
+            await onSnapshot(collection(db, 'JOYAS'), (query) =>{
+                setListarJoyas(query.docs.map((doc)=>({...doc.data(), id:doc.id})))
+            })
 
-    useEffect(()=>{
-        const obtenerDatos = async() => {
-            try{
-                await onSnapshot(collection(db, 'JOYAS'), (query) =>{
-                    setJoyas(query.docs.map((doc)=>({...doc.data(), id:doc.id})))
-                })
-
-            }catch(error){
-                console.log(error)
-            }
+        }catch(error){
+            console.log(error)
         }
-    })
-  
-  
-    return (
-    <div className='container mt-5'>
-        <h1 className='text-center'> JOYERIA BO </h1>
+      }
+
+      const buscarJoya = () =>{
+        const joyaEncontrada = listarJoyas.find((joya) => joya.material === material && joya.dije === dije && joya.tipo === tipo)
+        if (joyaEncontrada){
+          if(moneda === "pesos"){
+            setValorAPagar(joyaEncontrada.valor * cantidad * 5000);
+          }else{
+            setValorAPagar(joyaEncontrada.valor * cantidad );
+          }
+        }
+      }
+      obtenerDatos();
+      if(material && dije && tipo && cantidad && moneda){
         
+        buscarJoya()
+      }
+    }, [material,dije,tipo,cantidad,moneda]);
+    
+    
+   
+    return (
+    <div className='container'>
+       <h1 className="text-center">JOYERIA BO</h1>
     </div>
         
   
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Formulario from './components/formulario'
+import Formulario from './components/Formulario'
 import './index.css'
 
 
