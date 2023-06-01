6f6dd78249ab89bbc4369fadaffd5856287766ae
Bernardo otero <bernardooteroaj@gmail.com>
2023-06-01 13:00:17 -0500

    decimo duplicacion formulario

---
+++ src/components/Form2.jsx
@@ -0,0 +1,100 @@
+import React, { useEffect, useState } from 'react'
+import { db } from '../Firebase';
+import { collection, doc, addDoc, onSnapshot, deleteDoc, updateDoc } from 'firebase/firestore'
+
+const Form2 = () => {
+    const [cantidad, setCantidad] = useState('');
+    const [material, setMaterial] = useState('');
+    const [dije, setDije] = useState('');
+    const [tipo, setTipo] = useState('');
+    const [moneda, setMoneda] = useState('');
+    const [valorAPagar, setValorAPagar] = useState('');
+    const [listarJoyas, setListarJoyas] = useState([])
+  
+    useEffect(() => {
+      const obtenerDatos = async() =>{
+        try{
+            await onSnapshot(collection(db, 'JOYAS'), (query) =>{
+                setListarJoyas(query.docs.map((doc)=>({...doc.data(), id:doc.id})))
+            })
+
+        }catch(error){
+            console.log(error)
+        }
+      }
+
+      const buscarJoya = () =>{
+        const joyaEncontrada = listarJoyas.find((joya) => joya.material === material && joya.dije === dije && joya.tipo === tipo)
+        if (joyaEncontrada){
+          if(moneda === "pesos"){
+            setValorAPagar(joyaEncontrada.valor * cantidad * 5000);
+          }else{
+            setValorAPagar(joyaEncontrada.valor * cantidad );
+          }
+        }
+      }
+      obtenerDatos();
+      if(material && dije && tipo && cantidad && moneda){
+        
+        buscarJoya()
+      }
+    }, [material,dije,tipo,cantidad,moneda]);
+    
+    
+   
+    return (
+    <div className='container d-flex justify-content-center align-items-center my-5'>
+      <div className='formulario-container'>
+        <h1 className="text-center">JOYERIA  BO</h1>
+        <form >
+          <div className="form-group">
+            <label htmlFor="material">Material:</label>
+            <select className="form-control" id="material" value={material} onChange={(e) => setMaterial(e.target.value)}>
+              <option value="">Seleccione un material</option>
+              <option value="cuero">Cuero</option>
+              <option value="cuerda">Cuerda</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <label htmlFor="dije">Dije:</label>
+            <select className="form-control" id="dije" value={dije} onChange={(e) => setDije(e.target.value)} >
+              <option value="">Seleccione un dije</option>
+              <option value="martillo">Martillo</option>
+              <option value="ancla">Ancla</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <label htmlFor="tipo">Tipo:</label>
+            <select className="form-control" id="tipo" value={tipo} onChange={(e) => setTipo(e.target.value)} >
+              <option value="">Seleccione un tipo</option>
+              <option value="oro">Oro</option>
+              <option value="plata">Plata</option>
+              <option value="niquel">Niquel</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <label htmlFor="cantidad">Cantidad:</label>
+            <input type="number" className="form-control" id="cantidad" value={cantidad} onChange={(e) => setCantidad(e.target.value)}/>
+          </div>
+          <div className="form-group">
+            <label htmlFor="moneda">Moneda:</label>
+            <select className="form-control"  id="moneda" value={moneda} onChange={(e) => setMoneda(e.target.value)}>
+              <option value="">Seleccione una moneda</option>
+              <option value="dolares">Dólares</option>
+              <option value="pesos">Pesos</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <label htmlFor="valorAPagar">Valor a pagar:</label>
+            <input type="text" className="form-control" id="valorAPagar" value={valorAPagar} disabled />
+          </div>
+        </form>
+      </div>
+    </div>
+        
+  
+  
+    )
+}
+
+export default Form2
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Formulario from './components/Formulario'
 import './index.css'
+import Form2 from './components/Form2'
 
 
 
@@ -9,6 +10,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Formulario/>
+    <Form2/>
   </React.StrictMode>,
 )
